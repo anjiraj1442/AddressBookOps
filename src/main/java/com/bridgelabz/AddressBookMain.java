26c069e82441f3cbe7f70b3558bd42e8100a9b7b
anjirajardula <anjirajardula@gmail.com>
2021-10-27 23:07:46 +0530

    Added:Adding the multiple contacts

--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -16,26 +16,39 @@ public class AddressBookMain {
 
         System.out.println("Welcome to Address Book Problem \n");
         Scanner sc = new Scanner(System.in);
-        NewAddressBook person = new NewAddressBook();
-        person.addContact();
-        System.out.println(person.newPerson);
-        System.out.println("Enter the choice \n 1. Edit \n 2. Delete");
-        int option = sc.nextInt();
-        switch (option){
-            case 1:
-                person.editContact();
-                System.out.println("You have Entered following data");
-                System.out.println(person.newPerson);
-                System.out.println("Thank you for Using the Address book");
-                break;
 
-            case 2:
-                person.deleteContact();
-                System.out.println("Address Book details :" + person.newPerson);
-                break;
-        }
+        NewAddressBook person = new NewAddressBook();
+        while (true) {
+            System.out.println("Enter the choice \n 1.add \n 2.edit \n 3.delete");
+            int option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("Enter no of contacts you want create in list");
+                    int choose = sc.nextInt();
+                    for (int i = 0; i < choose; i++) {
+                        person.addContact();
+                    }
+                    break;
+
+                case 2:
+                    person.editContact();
+                    System.out.println("You have Entered following data");
+                    System.out.println(person.newPerson);
+                    System.out.println("Thank you for Using the Address book");
+                    break;
+
+                case 3:
+                    person.deleteContact();
+                    System.out.println("Address Book details :" + person.newPerson);
+                    break;
+                default:
+
+                    System.out.println("Enter correct the choice");
+            }
         }
     }
+}
 
 
 
